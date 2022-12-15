5e5bba073ccebdf20da6827ec470a7b52eb9e3ba
Redegit <dima070603@gmail.com>
2022-12-15 23:46:16 +0300

    add eng and spec. symbols

--- main.py
+++ main.py
@@ -4,14 +4,31 @@ import numpy as np
 class Playfair:
     """Класс для кодирования/декодирования при помощи шифра Плейфера"""
 
-    alphabet = [chr(i) for i in range(1040, 1072)]
+    alphabet = [chr(i) for i in range(1040, 1072)]  # Кириллица
+    # alphabet += [" ", "_", "-", ".", ",", "?"]  # Специальные символы
+    # alphabet += [chr(i) for i in range(65, 91)]  # Латиница
+    alpha_size = len(alphabet)
+    shape = (0, 0)
+
+    @staticmethod
+    def generate_key() -> str:
+        """Генератор случайного ключа"""
+
+        # Подбор случайной длины
+        n = np.random.randint(1, Playfair.alpha_size)
+
+        # Случайный выбор n букв из алфавита и запись в строку
+        key = "".join(np.random.choice(Playfair.alphabet, n))
+
+        return key
 
     @staticmethod
     def find_indexes(bigram: str, matrix: np.array) -> tuple:
         """Поиск индексов элементов биграммы в матрице"""
+
         i1, i2 = None, None
-        for i in range(4):
-            for j in range(8):
+        for i in range(Playfair.shape[0]):
+            for j in range(Playfair.shape[1]):
                 if matrix[i][j] == bigram[0]:
                     i1 = (i, j)
                 if matrix[i][j] == bigram[1]:
@@ -23,8 +40,9 @@ class Playfair:
     def make_matrix(key: str) -> np.array:
         """Создание матрицы шифра на основе ключевого слова и русского алфавита"""
 
-        # Удаление повторов букв из ключа
-        unique_key = list(map(lambda x: x.upper(), list(dict.fromkeys(key))))
+        # Удаление повторов букв из ключа и неалфавитных символов
+        unique_key = list(map(lambda x: x.upper(), list(dict.fromkeys(
+            [s.upper() for s in key if s.upper() in Playfair.alphabet]))))
 
         # Вычитание ключа из алфавита
         alpha_without_key = [item for item in Playfair.alphabet if item not in frozenset(unique_key)]
@@ -34,13 +52,24 @@ class Playfair:
 
         # Создание матрицы
         matrix = np.array(matrix_data)
-        matrix.shape = (4, 8)
+
+        # Подбор размеров матрицы, чтобы она была более квадратной
+        for i in range(int(Playfair.alpha_size ** 0.5), Playfair.alpha_size + 1):
+            if Playfair.alpha_size % i == 0:
+                Playfair.shape = (Playfair.alpha_size // i, i)
+                break
+
+        matrix.shape = Playfair.shape
 
         return matrix
 
     @staticmethod
-    def encrypt(message: str, key: str) -> str:
+    def encrypt(message: str, key=None) -> str:
         """Шифрование переданной строки с использованием ключа"""
+
+        # Если ключ не введён, генерируется случайный
+        if key is None:
+            key = Playfair.generate_key()
 
         # Создание матрицы
         matrix = Playfair.make_matrix(key)
@@ -65,8 +94,12 @@ class Playfair:
             else:
                 break
 
-        # print(matrix)
-        # print(bigram_list)
+        print("\n" + "-" * 10 + " encrypting " + "-" * 10)
+        print(f"{key = }")
+        print(f"matrix:\n{matrix}")
+        print(f"shape = {Playfair.shape}")
+        print(f"{bigram_list = }")
+        print("-" * 32 + "\n")
 
         # Шифрование сообщения при помощи матрицы
         result_str = ""
@@ -74,11 +107,11 @@ class Playfair:
             i1, i2 = Playfair.find_indexes(big, matrix)
 
             if i1[0] == i2[0]:
-                l1 = matrix[i1[0]][(i1[1] + 1) % 8]
-                l2 = matrix[i2[0]][(i2[1] + 1) % 8]
+                l1 = matrix[i1[0]][(i1[1] + 1) % Playfair.shape[1]]
+                l2 = matrix[i2[0]][(i2[1] + 1) % Playfair.shape[1]]
             elif i1[1] == i2[1]:
-                l1 = matrix[(i1[0] + 1) % 4][i1[1]]
-                l2 = matrix[(i2[0] + 1) % 4][i2[1]]
+                l1 = matrix[(i1[0] + 1) % Playfair.shape[0]][i1[1]]
+                l2 = matrix[(i2[0] + 1) % Playfair.shape[0]][i2[1]]
             else:
                 l1 = matrix[i2[0]][i1[1]]
                 l2 = matrix[i1[0]][i2[1]]
@@ -97,8 +130,12 @@ class Playfair:
         message = message.upper()
         bigram_list = [message[i] + message[i + 1] for i in range(0, len(message), 2)]
 
-        # print(matrix)
-        # print(bigram_list)
+        print("\n" + "-" * 10 + " decrypting " + "-" * 10)
+        print(f"{key = }")
+        print(f"matrix:\n{matrix}")
+        print(f"shape = {Playfair.shape}")
+        print(f"{bigram_list = }")
+        print("-" * 32 + "\n")
 
         # Дешифровка биграмм при помощи матрицы
         result_str = ""
